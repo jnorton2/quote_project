3d9f501711f3b4479fe6ea74f8131bd306594fff
Josh Norton <joshrnorton75@gmail.com>
2017-04-05 23:53:59 -0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+To get this to run at bash startup add the following to the .bashrc file in your home directory 
+
+'''clear
+ECHO "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+python ~/python/quoteProject/printQuote.py
+ECHO "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+'''
---
+++ printQuote.py
@@ -0,0 +1,20 @@
+import random, re, subprocess, textwrap
+
+with open('/Users/joshnorton/python/quoteProject/quotes.txt','r') as file:
+	names = []
+	quotes = []
+	for line in file:
+	    names.append(line.split('|')[0])
+	    quotes.append(line.split('|')[1])
+
+
+	name_quote_map = zip(names, quotes)
+	random.shuffle(name_quote_map)
+
+	indicies = [i for i in range(0, len(names))]
+	random.shuffle(indicies)
+	n = indicies[0]
+
+
+	s = name_quote_map[n][1] + "-" +  name_quote_map[n][0]
+	print textwrap.fill(s)
